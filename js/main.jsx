1f6e51a37391b0d84dcea543d72876444b6535bf
Max <oc.kode@gmail.com>
2022-03-05 22:47:19 +0100

    maj

--- js/main.jsx
+++ js/main.jsx
@@ -1,37 +1,37 @@
-const TODOS = [
-  {
-    id: 1,
-    name: "todo 1",
-    isCompleted: false,
-    isDeleted: false,
-  },
-  {
-    id: 2,
-    name: "todo 2",
-    isCompleted: false,
-    isDeleted: false,
-  },
-  {
-    id: 3,
-    name: "todo 3",
-    isCompleted: false,
-    isDeleted: false,
-  },
-  {
-    id: 4,
-    name: "todo 4",
-    isCompleted: false,
-    isDeleted: false,
-  },
-  {
-    id: 5,
-    name: "todo 5",
-    isCompleted: false,
-    isDeleted: false,
-  },
-];
-
 const App = () => {
+  const TODOS = [
+    {
+      id: 1,
+      name: "todo 1",
+      isCompleted: false,
+      isDeleted: false,
+    },
+    {
+      id: 2,
+      name: "todo 2",
+      isCompleted: false,
+      isDeleted: false,
+    },
+    {
+      id: 3,
+      name: "todo 3",
+      isCompleted: false,
+      isDeleted: false,
+    },
+    {
+      id: 4,
+      name: "todo 4",
+      isCompleted: false,
+      isDeleted: false,
+    },
+    {
+      id: 5,
+      name: "todo 5",
+      isCompleted: false,
+      isDeleted: false,
+    },
+  ];
+
   return <Todos allTodos={TODOS} />;
 };
 
