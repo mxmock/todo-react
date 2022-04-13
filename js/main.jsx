5769a56eba89dd29f4cb2b9633e8876726c65d68
Max <oc.kode@gmail.com>
2022-04-13 12:01:38 +0200

    fix

--- js/main.jsx
+++ js/main.jsx
@@ -4,31 +4,26 @@ const App = () => {
       id: 1,
       name: "todo 1",
       isCompleted: false,
-      isDeleted: false,
     },
     {
       id: 2,
       name: "todo 2",
       isCompleted: false,
-      isDeleted: false,
     },
     {
       id: 3,
       name: "todo 3",
       isCompleted: false,
-      isDeleted: false,
     },
     {
       id: 4,
       name: "todo 4",
       isCompleted: false,
-      isDeleted: false,
     },
     {
       id: 5,
       name: "todo 5",
       isCompleted: false,
-      isDeleted: false,
     },
   ];
 
