96b7759be4b348ea6e9e316b0efe9cdd9b004aab
Max <oc.kode@gmail.com>
2022-04-28 13:59:10 +0200

    step_9 first

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,6 +7,7 @@ import trashIcon from "../../img/trash-outline.svg";
 import FloatingBtn from "../floating-btn/FloatingBtn";
 import TodosFilter from "../todos-filter/TodosFilter";
 import TodoAddForm from "../todo-add-form/TodoAddForm";
+import { useSelector } from "react-redux";
 
 const App = () => {
   /*************************** COMPLETE ***************************/
@@ -38,6 +39,7 @@ const App = () => {
   /*************************** STATES ***************************/
 
   const [todos, setTodos] = useState(TODOS);
+  const state = useSelector((state) => state.todos);
   const [filter, setFilter] = useState(FILTER.ALL);
   const [filteredTodos, setFilteredTodos] = useState(TODOS);
 
@@ -51,6 +53,7 @@ const App = () => {
         ? todos.filter((t) => !t.isCompleted)
         : [...todos];
     });
+    console.log(state);
   }, [filter, todos]);
 
   /*************************** HTML ***************************/
